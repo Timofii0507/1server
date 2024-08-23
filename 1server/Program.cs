dc00e146bc1478b8f32b49873baaf064815abb03
Юрков Тимофій Олександрович <145855201+Timofii0507@users.noreply.github.com>
2024-08-23 10:40:39 +0300

    hm/ex-3

--- 1server/Program.cs
+++ 1server/Program.cs
@@ -2,14 +2,15 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 
 class Server
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
         Console.ForegroundColor = ConsoleColor.DarkBlue;
         Console.CursorVisible = false;
-        IPAddress ipAddress = IPAddress.Parse("192.168.0.103");
+        IPAddress ipAddress = IPAddress.Parse("192.168.0.1");
         int port = 11000;
         IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
 
@@ -24,43 +25,31 @@ class Server
 
             while (true)
             {
-                Socket handler = listener.Accept();
-                string data = null;
-
-                byte[] bytes = new byte[1024];
-                int bytesRec = handler.Receive(bytes);
-                data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
-
-                string response;
-                if (data.ToLower().Contains("time"))
-                {
-                    response = DateTime.Now.ToString("HH:mm:ss");
-                }
-                else if (data.ToLower().Contains("date"))
-                {
-                    response = DateTime.Now.ToString("yyyy-MM-dd");
-                }
-                else
-                {
-                    response = "Invalid request";
-                }
-
-                byte[] msg = Encoding.UTF8.GetBytes(response);
-                handler.Send(msg);
-
-                Console.WriteLine($"At {DateTime.Now.ToShortTimeString()} received from {((IPEndPoint)handler.RemoteEndPoint).Address}: {data}");
-                Console.WriteLine($"Sent response: {response}");
-
-                handler.Shutdown(SocketShutdown.Both);
-                handler.Close();
+                Socket handler = await listener.AcceptAsync();
+                _ = HandleClientAsync(handler);
             }
         }
         catch (Exception e)
         {
             Console.WriteLine(e.ToString());
         }
+    }
 
-        Console.WriteLine("\nPress any key to exit...");
-        Console.ReadKey();
+    private static async Task HandleClientAsync(Socket handler)
+    {
+        string data = null;
+
+        byte[] bytes = new byte[1024];
+        int bytesRec = await handler.ReceiveAsync(new ArraySegment<byte>(bytes), SocketFlags.None);
+        data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+
+        Console.WriteLine($"At {DateTime.Now.ToShortTimeString()} received from {((IPEndPoint)handler.RemoteEndPoint).Address}: {data}");
+
+        string response = "Hello, client!";
+        byte[] msg = Encoding.UTF8.GetBytes(response);
+        await handler.SendAsync(new ArraySegment<byte>(msg), SocketFlags.None);
+
+        handler.Shutdown(SocketShutdown.Both);
+        handler.Close();
     }
 }
